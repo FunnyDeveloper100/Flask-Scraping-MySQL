34263b2f069f80bdc28e8aeb23009516aa435f97
codemaster08240328 <codemaster08240328@gmail.com>
2019-07-22 11:44:18 -0300

    register blueprint domain app with url prefix domain

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from app.db import db
 from flask import Flask, render_template, redirect
 from flask_migrate import Migrate
 from config import settings
+from app.domain import domain_app
 
 
 """ create flask app """
@@ -18,7 +19,7 @@ migrate = Migrate(application, db)
 
 
 """ register bluprint """
-
+application.register_blueprint(domain_app.domain_app)
 
 """ error page """
 @application.errorhandler(404)
