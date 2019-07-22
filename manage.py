bfcf382887d9a6edaa5e3763bc14872996219e65
codemaster08240328 <codemaster08240328@gmail.com>
2019-07-22 12:01:31 -0300

    add manager.py to manage the app with database

---
+++ manage.py
@@ -0,0 +1,10 @@
+from flask_script import Manager
+from flask_migrate import MigrateCommand
+from app import application
+
+
+manager = Manager(application)
+manager.add_command('db', MigrateCommand)
+
+if __name__='__name__':
+    manager.run()
